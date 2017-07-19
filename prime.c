48c88fd0ec4b1fde8206d2f4e544ccb257767610
pramodsharma92 <pramod.a.sharma46@gmail.com>
2017-07-19 10:46:15 -0700

    Update prime.c

--- prime.c
+++ prime.c
@@ -3,7 +3,7 @@ void prime()
 {
     int n, i, flag = 0;
 
-    printf("Enter a positive integer: ");
+    printf("Enter a positive integer: /n");
     scanf("%d",&n);
 
     for(i=2; i<=n/2; ++i)
@@ -17,9 +17,9 @@ void prime()
     }
 
     if (flag==0)
-        printf("%d is a prime number.",n);
+        printf("/n%d is a prime number.",n);
     else
-        printf("%d is not a prime number.",n);
+        printf("/n%d is not a prime number.",n);
     
  // return 0;
 }
